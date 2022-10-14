8aeefd56d12d8872325005f6e3e4010772646e1d
feiwhoami <feiwhoami@icloud.com>
2022-10-14 15:33:18 -0700

    463

---
+++ leetcode/463_island_perimeter.py
@@ -0,0 +1,35 @@
+"""
+You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1.
+The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+Example 1:
+Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+Output: 16
+Explanation: The perimeter is the 16 yellow stripes in the image above.
+
+Example 2:
+Input: grid = [[1]]
+Output: 4
+
+Example 3:
+Input: grid = [[1,0]]
+Output: 4
+"""
+
+from typing import List
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        res, rows, cols = 0, len(grid), len(grid[0])
+        for i in range (rows):
+            for j in range (cols):
+                if grid[i][j]:
+                    res += 4
+                    if i > 0 and grid[i - 1][j]:
+                        res -= 2
+                    if j > 0 and grid[i][j - 1]:
+                        res -= 2
+
+        return res
