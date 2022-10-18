a8469bfcaef5617a278236074529f9cec720afea
feiwhoami <feiwhoami@icloud.com>
2022-10-18 15:05:52 -0700

    496

---
+++ leetcode/496_next_greater_element_I.py
@@ -0,0 +1,44 @@
+"""
+The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+Example 1:
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+Output: [-1,3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+- 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+- 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+
+Example 2:
+Input: nums1 = [2,4], nums2 = [1,2,3,4]
+Output: [3,-1]
+Explanation: The next greater element for each value of nums1 is as follows:
+- 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+- 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+"""
+
+from typing import List
+
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        m, s = {}, []
+        for num in nums2:
+            while s and s[-1] < num:
+                m[s.pop()] = num
+            s.append(num)
+
+        res = []
+        for num in nums1:
+            if num not in m:
+                res.append(-1)
+            else:
+                res.append(m[num])
+
+        return res
+
+# Run and Test
+s = Solution()
+print(s.nextGreaterElement([4,1,2], [1,3,4,2]))
