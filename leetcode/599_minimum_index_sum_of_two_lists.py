e965f88c0cf6262cd1d95b7b62c4a7d8b1538cb9
feiwhoami <feiwhoami@icloud.com>
2022-11-10 15:42:05 -0800

    599

---
+++ leetcode/599_minimum_index_sum_of_two_lists.py
@@ -0,0 +1,45 @@
+"""
+Given two arrays of strings list1 and list2, find the common strings with the least index sum.
+A common string is a string that appeared in both list1 and list2.
+A common string with the least index sum is a common string such that if it appeared at list1[i] and list2[j] then i + j should be the minimum value among all the other common strings.
+Return all the common strings with the least index sum. Return the answer in any order.
+
+Example 1:
+Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+Output: ["Shogun"]
+Explanation: The only common string is "Shogun".
+
+Example 2:
+Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["KFC","Shogun","Burger King"]
+Output: ["Shogun"]
+Explanation: The common string with the least index sum is "Shogun" with index sum = (0 + 1) = 1.
+
+Example 3:
+Input: list1 = ["happy","sad","good"], list2 = ["sad","happy","good"]
+Output: ["sad","happy"]
+Explanation: There are three common strings:
+"happy" with index sum = (0 + 1) = 1.
+"sad" with index sum = (1 + 0) = 1.
+"good" with index sum = (2 + 2) = 4.
+The strings with the least index sum are "sad" and "happy".
+"""
+
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        count = {}
+        for s1 in list1:
+            if s1 in list2:
+                sum = list1.index(s1) + list2.index(s1)
+                if sum not in count:
+                    count[sum] = [s1]
+                else:
+                    count[sum].append(s1)
+
+        return count[min(count.keys())]
+
+# Run and Test
+s = Solution()
+print(s.findRestaurant(["Shogun","Tapioca Express","Burger King","KFC"], ["KFC","Shogun","Burger King"]))
